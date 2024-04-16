1bbef837c0f697a753833ec0950dd4fd9f4526ed
Kevin Delfour <kevin.delfour@maxds.fr>
2024-04-16 14:33:10 +0200

    :bug: Fix fetch events data

--- components/Events.vue
+++ components/Events.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 defineProps<{ content?: JSON }>()
-const { data } = await useFetch('/api/fetchEvents')
+const { data, pending } = useFetch('/api/fetchEvents')
 </script>
 
 <template>
@@ -16,51 +16,53 @@ const { data } = await useFetch('/api/fetchEvents')
         }"
         class="m-5"
       >
-        <template v-if="data.pagination['object_count'] == 0">
-          <UAlert
-            :icon="content.message.empty.icon"
-            :ui="{ wrapper: 'w-3/6' }"
-            :title="content.message.empty.title"
-            :description="content.message.empty.description"
-            color="red"
-            variant="solid"
-            class="ml-auto mr-auto"
-          />
-        </template>
-        <template v-else>
-          <ULandingGrid>
-            <ULandingCard
-              v-for="event in data.events"
-              :key="event.id"
-              target="_blank"
-              :to="event.url"
-              class="col-span-6"
-            >
-              <template #title>
-                <NuxtImg sizes="100vw" :src="event.logo.url" />
-                <h1 class="text-xl">
-                  {{ event.name.text }}
-                </h1>
-              </template>
-              <template #description>
-                <em>
-                  Le {{ new Date(event.start.local).toLocaleDateString('fr-FR') }} à {{ new
-                    Date(event.start.local).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' }) }}<br>
-                  Nombre de place disponible : {{ event.capacity }}
-                </em>
+        <div v-if="!pending">
+          <template v-if="data & data.pagination & data.pagination['object_count'] == 0">
+            <UAlert
+              :icon="content.message.empty.icon"
+              :ui="{ wrapper: 'w-3/6' }"
+              :title="content.message.empty.title"
+              :description="content.message.empty.description"
+              color="red"
+              variant="solid"
+              class="ml-auto mr-auto"
+            />
+          </template>
+          <template v-else>
+            <ULandingGrid>
+              <ULandingCard
+                v-for="event in data.events"
+                :key="event.id"
+                target="_blank"
+                :to="event.url"
+                class="col-span-6"
+              >
+                <template #title>
+                  <NuxtImg sizes="100vw" :src="event.logo.url" />
+                  <h1 class="text-xl">
+                    {{ event.name.text }}
+                  </h1>
+                </template>
+                <template #description>
+                  <em>
+                    Le {{ new Date(event.start.local).toLocaleDateString('fr-FR') }} à {{ new
+                      Date(event.start.local).toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' }) }}<br>
+                    Nombre de place disponible : {{ event.capacity }}
+                  </em>
 
-                <p class="text-lg my-3">
-                  {{ event.description.text }}
-                </p>
+                  <p class="text-lg my-3">
+                    {{ event.description.text }}
+                  </p>
 
-                <p />
-                <UButton block :to="event.url">
-                  Réserver ma place
-                </UButton>
-              </template>
-            </ULandingCard>
-          </ULandingGrid>
-        </template>
+                  <p />
+                  <UButton block :to="event.url">
+                    Réserver ma place
+                  </UButton>
+                </template>
+              </ULandingCard>
+            </ULandingGrid>
+          </template>
+        </div>
       </ULandingSection>
     </div>
   </div>
