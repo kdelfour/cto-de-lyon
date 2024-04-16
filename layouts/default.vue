3776dd0a069fd48bb814be80e326df9742b82c34
Kevin Delfour <kevin.delfour@maxds.fr>
2024-04-16 10:50:05 +0200

    :feat: Change SSR mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,6 @@
 <script setup>
 import { SpeedInsights } from '@vercel/speed-insights/nuxt'
-
 useAppConfig()
-
-useHead({
-  htmlAttrs: {
-    lang: 'fr'
-  },
-  link: [
-    { rel: 'apple-touch-icon', size: '180x180', href: '/apple-touch-icon.png' },
-    { rel: 'icon', type: 'image/png', size: '32x32', href: '/favicon-32x32.png' },
-    { rel: 'icon', type: 'image/png', size: '16x16', href: '/favicon-16x16.png' },
-    { rel: 'manifest', href: '/site.webmanifest' },
-    { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
-  ],
-  meta: [
-    { name: 'msapplication-TileColor', content: '#da532c' },
-    { name: 'theme-color', content: '#ffffff' }
-  ],
-  title: 'CTO de Lyon',
-  script: [
-    {
-      innerHTML: '(function(d, w, c) { w.BrevoConversationsID = "65d06b6dcede7a2bf61e1f6f"; w[c] = w[c] || function() { (w[c].q = w[c].q || []).push(arguments); }; var s = d.createElement("script"); s.async = true; s.src = "https://conversations-widget.brevo.com/brevo-conversations.js"; if (d.head) d.head.appendChild(s); })(document, window, "BrevoConversations");',
-      body: true
-    }
-  ]
-})
 </script>
 
 <template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,68 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  app: {
+    head: {
+      title: 'CTO de Lyon',
+      htmlAttrs: { lang: 'fr' },
+      meta: [
+        {
+          name: 'description',
+          content:
+            "Une communauté dynamique de leaders technologiques qui façonnent l'avenir à travers collaboration et innovation"
+        },
+        { name: 'theme-color', content: '#ffffff' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+      ],
+      script: [
+        {
+          innerHTML:
+            '(function(d, w, c) { w.BrevoConversationsID = "65d06b6dcede7a2bf61e1f6f"; w[c] = w[c] || function() { (w[c].q = w[c].q || []).push(arguments); }; var s = d.createElement("script"); s.async = true; s.src = "https://conversations-widget.brevo.com/brevo-conversations.js"; if (d.head) d.head.appendChild(s); })(document, window, "BrevoConversations");',
+          tagPosition: 'bodyClose'
+        }
+      ],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          size: '180x180',
+          href: '/apple-touch-icon.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          size: '32x32',
+          href: '/favicon-32x32.png'
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          size: '16x16',
+          href: '/favicon-16x16.png'
+        },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#5bbad5' }
+      ],
+      charset: 'utf-8'
+    }
+  },
   devtools: { enabled: true },
   routeRules: { '/': { prerender: true } },
   nitro: { preset: 'vercel-edge' },
   extends: ['@nuxt/ui-pro'],
-  modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxt/image',
-    '@nuxtjs/seo'
-  ],
-  colorMode: {
-    preference: 'dark'
-  },
+  modules: ['@nuxt/content', '@nuxt/ui', '@nuxt/image'],
+  colorMode: { preference: 'dark' },
   ui: {
     global: true,
     icons: ['simple-icons', 'heroicons']
   },
   image: {},
-  site: {
-    url: 'https://cto-de-lyon.fr/',
-    name: 'CTO de Lyon',
-    description: 'Une communauté dynamique de leaders technologiques qui façonnent l\'avenir à travers collaboration et innovation',
-    defaultLocale: 'fr'
-  },
+  // site: {
+  //   url: 'https://cto-de-lyon.fr/',
+  //   name: 'CTO de Lyon',
+  //   description:
+  //     "Une communauté dynamique de leaders technologiques qui façonnent l'avenir à travers collaboration et innovation",
+  //   defaultLocale: 'fr'
+  // },
   runtimeConfig: {
     EVENTBRITE_TOKEN: process.env.EVENTBRITE_TOKEN,
     EVENTBRITE_ORG_ID: process.env.EVENTBRITE_ORG_ID
